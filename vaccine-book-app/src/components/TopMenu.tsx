fbd3dce6ae379124b8472ac795b5e5c539f6e482
RefinXD <The32069@gmail.com>
2023-10-20 16:21:13 +0700

    added code for assignment 10

--- vaccine-book-app/src/components/TopMenu.tsx
+++ vaccine-book-app/src/components/TopMenu.tsx
@@ -2,16 +2,28 @@ import TopMenuItem from './TopMenuItem';
 import styles from './topmenu.module.css'
 import Image from 'next/image';
 import Link from 'next/link';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 
-export default function TopMenu(){
+
+
+export default async function TopMenu(){
+
+    const session = await getServerSession(authOptions)
+    console.log(session)
     return (
         <div className={styles.menucontainer}>
                 <Image src={'/img/vaccinelogo.png'} className={styles.logoimg}
                 alt='logo' width = {100} height={100} sizes='100vh'/>
 
             <TopMenuItem title='Booking' pageRef='/booking'/>
-        </div>
-        
-    )
+            {
+                session? <Link href="/api/auth/signout"><div className='flex items-center absolute left-0 h-full px-2
+                absolute left-0 text-cyan-600 text-sm'>Sign-Out of {session.user?.name}</div></Link>
+                : <Link href="/api/auth/signin"><div className='flex items-center absolute left-0 h-full px-2
+                absolute left-0 text-cyan-600 text-sm'>Sign-In</div></Link>
+            }
+        </div> 
+    );
 }
